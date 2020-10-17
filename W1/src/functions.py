5c013b114983afe65721ffb851e81c787c67b246
Lena <maguitg@gmail.com>
2020-10-17 13:40:24 +0200

    No lo había subido baia

--- W1/src/functions.py
+++ W1/src/functions.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """ MCV - M1:  Introduction to human and computer vision
     Week 1 - Content Based Image Retrieval
@@ -9,31 +9,32 @@
 
     functions.py: TODO
 """
-import itertools
-from collections import OrderedDict
 
 """ Imports """
 import cv2
 import numpy as np
 import math
+import itertools
+import pickle
+import os
+from collections import OrderedDict
+from matplotlib import pyplot as plt
+import ml_metrics as metrics
+#from mpl_toolkits.mplot3d import Axes3D
+#from matplotlib import cm
+#from matplotlib import colors 
 
 """ Constants """
-# here you can define all the constants you might need
 
 """ Global variables """
-# here you can definde global variables -> IF NEEDED, try not to use them at all costs
 
 """ Classes """
-# if needed
 
 """ Functions """
 """ ----- T1 - DESCRIPTORS ----- """
 def transform_color(image, color_space):
     """
     Computes image descriptors
-
-    :param image: TODO
-    :param color_space: TODO
     """
     conv_color = {
         "Gray": cv2.COLOR_RGB2GRAY,
@@ -47,17 +48,112 @@ def transform_color(image, color_space):
 
     return cv2.cvtColor(image, conv_color[color_space])
 
-def compute_gray_histogram(key,img):
+def compute_1D_histogram(img, fn, plot, store, mask=None):
     """
-    Computes the gray-scale normalized histogram
-    :param img: 
-    :return: 
+    Computes the gray-scale histogram
     """
-    hist = dict()
-    gray = transform_color(img[key],"Gray")
-    hist= cv2.calcHist([gray], [0], None, [256], [0, 256])  # compute the histogram
-    hist = cv2.normalize(hist, hist)  # normalize the values of the histogram
+
+    gray = transform_color(img, "Gray")
+    hist= cv2.calcHist([gray], [0], mask, [256], [0, 256])  # compute the histogram
+
+    if plot or store:
+        fig = plt.figure()
+        plt.title("1D Grayscale histogram of "+fn)
+        plt.xlabel("Bins")
+        plt.ylabel("# of pixels")
+        plt.plot(hist)
+        plt.xlim([0,256])
+        if plot:
+            plt.show()
+        if store:
+            plt.savefig('../results/1D_hist/'+fn)
+        plt.close(fig)
+
     return hist
+
+def compute_2D_histogram(img, fn, plot, store, mask=None):
+    """
+    Computes H & S histogram
+    """
+    hsv = transform_color(img, "HSV")
+    hist = cv2.calcHist([hsv], [0,1], mask, [180, 256], [0, 180, 0, 256])
+
+    if plot or store:
+        fig = plt.figure()
+        plt.title("2D HSV histogram of "+fn)
+        plt.xlabel("Saturation")
+        plt.ylabel("Hue")
+        plt.imshow(hist)
+        if plot:
+            plt.show()
+        if store:
+            plt.savefig('../results/2D_hist/'+fn)
+        plt.close(fig)
+
+    return hist
+
+def compute_RGB_3D_histogram(img, fn, plot, store, mask=None):
+    """
+    Computes RGB histogram
+    """
+    plt.close('all')
+    hist = dict()
+    b, g, r = cv2.split(img)
+
+    if plot or store:
+        fig = plt.figure(figsize=(8,4))
+        plt.title("3D RGB histogram of "+fn)
+        plt.xlabel('Bins')
+        plt.ylabel('# of pixels')
+        plt.xlim([0,256])
+
+    for x, c in zip([b,g,r], ["b", "g", "r"]):
+        h = cv2.calcHist([x], [0], None, [256], [0,256])
+        hist[c] = h
+
+        if plot or store:
+            plt.plot(h, color=c)
+
+    if plot or store:
+        if plot:
+            plt.show()
+        if store:
+            plt.savefig('../results/3D_hist/'+fn)
+        plt.close(fig)
+
+    return hist
+
+def task1(images, descriptor, plot, store, masks = None):
+    """
+    Computes all the histograms from the images of a folder
+
+    :param images: src images
+    :param descriptor: kind of descriptor we want to extract from the images
+    :param plot: boolean that indicates if plots of the descriptors must be shown while on execution
+    :param store: boolean that indicates if plots must be stored in a result folder on the parent directory (must be created beforehand)
+    :return: histograms 
+    """
+    histograms = dict()
+    for fn in images:
+        img = images[fn]
+        
+        if masks is not None:
+            mask = masks[fn]
+        else:
+            mask = None
+
+        if descriptor == "1D_hist":
+            hist = compute_1D_histogram(img, fn, plot, store, mask)
+        elif descriptor == "2D_hist":
+            hist = compute_2D_histogram(img, fn, plot, store, mask)
+        elif descriptor == "3D_hist":
+            hist = compute_RGB_3D_histogram(img, fn, plot, store, mask)
+        else:
+            print("Incorrect descriptor")
+            return 
+        histograms[fn] = hist
+
+    return histograms
 
 """ ----- T2 - SIMILARITY MEASURES ----- """
 def check(h_dataset, h_query):
@@ -69,7 +165,6 @@ def check(h_dataset, h_query):
     """
     im_dataset, vals_dataset = np.shape(h_dataset)
     size_shape = np.shape(np.shape(h_query))[0]
-    
 
     # Case 1:
     if size_shape == 1:
@@ -78,7 +173,7 @@ def check(h_dataset, h_query):
 
     # Case 2:
     if size_shape > 1:
-        print("query histogram is more bigger")
+        print("query histogram is bigger")
     else:
         nims, nvals_query = np.shape(h_query)
 
@@ -91,39 +186,33 @@ def check(h_dataset, h_query):
     return h_query
 
 def euclidean(h_dataset, h_query):
-
    # h_query = check(h_dataset, h_query)
     distance = math.sqrt(sum((h_dataset - h_query)** 2))
     return distance
 
 def l1_distance(h_dataset, h_query):
-
     #h_query = check(h_dataset, h_query)
     distance = sum(np.absolute(h_dataset - h_query))
 
     return distance
 
 def x2_distance(h_dataset, h_query):
-
     h_query = check(h_dataset, h_query)
     distance = np.sum(np.power((h_dataset - h_query), 2)/ (h_dataset + h_query),axis=1,)
 
     return distance
 
 def hist_intersection(h_dataset, h_query):
-
     #h_query = check(h_dataset, h_query)
     distance = 1/(np.sum(np.minimum(h_dataset, h_query), axis=1))
     return distance
 
 def hellinger(h_dataset, h_query):
-
    # h_query = check(h_dataset, h_query)
     distance = sum(np.sqrt(h_dataset * h_query))
     return distance
 
 def kl_divergence(h_dataset, h_query):
-
     h_query = check(h_dataset, h_query)
     distance = np.sum(h_query * np.log((h_query) / (h_dataset)), axis=1,)
     return distance
@@ -131,9 +220,9 @@ def kl_divergence(h_dataset, h_query):
 def calculate_distances(h_dataset, h_query, mode):
     if mode == "euclidean":
         return euclidean(h_dataset, h_query)
-    elif mode == "l1_distance":
+    elif mode == "l1":
         return l1_distance (h_dataset, h_query)
-    elif mode == "x2_distance":
+    elif mode == "x2":
         return x2_distance(h_dataset, h_query)
     elif mode == "hist_intersection":
         return hist_intersection(h_dataset, h_query)
@@ -145,16 +234,32 @@ def calculate_distances(h_dataset, h_query, mode):
         raise Exception("Not function")
 
 """ ----- T3 - IMAGE COMPARISION  ----- """
-# these functions have to be called by T4a and T4b
-def calculate_diferences(h_dataset,h_query,mode):
+def calculate_diferences(h_dataset, h_query, mode):
     distances = dict()
     for query in h_query.keys():
         distance = dict()
         for dataset in h_dataset.keys():
-
             dist = calculate_distances(h_dataset[dataset],h_query[query],mode)
             distance[dataset] = dist
         distances[query] = distance
+
+    return distances
+
+def task3(images_bbdd, images, measure):
+    """
+    Computes the distance between two sets of images
+
+    :param images_bbdd: images from the databes 
+    :param images: images from qsd1
+    :param measure: measure we want to use to compute the distance
+    :return distances: returns all the distances between images where
+    """
+    bbdd_hist = task1(images_bbdd, "1D_hist", False, False)
+    qsd1_hist = task1(images, "1D_hist", False, False)
+
+    distances = calculate_diferences(bbdd_hist, qsd1_hist, measure)
+    #TODO poder enseñar los resultados al usuario de alguna manera como task 1 -> si no da tiempo suda
+    print("Not implemented how to show the results to the user")
 
     return distances
 
@@ -166,26 +271,166 @@ def get_top_k(differences,top_k,reverse = False):
     results = []
 
     for diff in differences.keys():
-        query.append(diff)
+        difference = []
+        difference.append(int(diff))
+        query.append(difference)
         if not reverse:
-            results.append(list(OrderedDict(sorted(differences[diff].items(), key=lambda t: t[1])).items())[:top_k])
+            resultados = list(OrderedDict(sorted(differences[diff].items(), key=lambda t: t[1])).items())[:top_k]
         else:
-            results.append(list(OrderedDict(sorted(differences[diff].items(), key=lambda t: t[1])).items())[-top_k:])
+            resultados = list(OrderedDict(sorted(differences[diff].items(), key=lambda t: t[1])).items())[-top_k:]
+        result = []
+        for res in resultados:
+            result.append(int(res[0]))
+        results.append(result)
+
     return query,results
 
+def task4(images_bbdd, images, measure):
+    """
+    Computes the top k imges and the MAP@k and stores it on a pkl file
+
+    :param images_bbdd: images from the databes 
+    :param images: images from qsd1
+    :param measure: measure we want to use to compute the distance
+    """
+    distances = task3(images_bbdd, images, measure)
+    if measure == 'euclidean':
+        query,top_results = get_top_k(distances,10) #get a top 10 results
+        with open('result_euclidean_T.pkl', 'wb') as output:  # write the results in a file
+            pickle.dump([query,top_results], output)
+        with open("result_euclidean_T.pkl", "rb") as fp:  # load the results in a file
+            [loaded_query,loaded_results] = pickle.load(fp)
+        #print(loaded_query,loaded_results)
+    elif measure == 'l1':
+        query, top_results = get_top_k(distances, 10)  # get a top 10 results
+        with open('result_l1_T', 'wb') as output:  # write the results in a file
+            pickle.dump([query, top_results], output)
+        with open("result_l1_T", "rb") as fp:  # load the results in a file
+            [loaded_query, loaded_results] = pickle.load(fp)
+    elif measure == 'hellinger':
+        query, top_results = get_top_k(distances, 10,True)  # get a top 10 results
+        with open('result_hellinger_T.pkl', 'wb') as output:  # write the results in a file
+            pickle.dump([query, top_results], output)
+        with open("result_hellinger_T.pkl", "rb") as fp:  # load the results in a file
+            [loaded_query, loaded_results] = pickle.load(fp)
+        print(loaded_query,loaded_results)
+    else:
+        raise Exception("Measure not supported for task 4")
+    
 
 """ MAP@k """
-# these functions have to be called by T6b
-
+def compute_mapk(test_results,develop_results):
+    result = metrics.mapk(test_results,develop_results,5)
+    return result
 
 """ ----- T5 - QS2  ----- """
-""" Background removal """
-# these functions have to be called by T6a
+def segment_paintings(images):
+    masks = dict()
 
-""" Descriptors """
+    low_color = np.array([0, 0, 70])
+    high_color = np.array([50, 70, 2000])
+    for fn in images:
+        img = images[fn]
 
+        blur = cv2.blur(img,(5,5))
+        blur0=cv2.medianBlur(blur,5)
+        blur1= cv2.GaussianBlur(blur0,(5,5),0)
+        blur2= cv2.bilateralFilter(blur1,9,75,75)
 
-""" ----- T6 - QS2 Evaluation  ----- """
-""" Top k """
+        hsv = cv2.cvtColor(blur2, cv2.COLOR_BGR2HSV)
 
-""" MAP@k """
+        res = cv2.inRange(hsv, low_color, high_color)
+        #kernel = np.ones((11,11),np.uint8)
+        #res = cv2.erode(res,kernel,iterations = 1)
+        #res = cv2.dilate(res,kernel,iterations = 1)
+        #cv2.imshow(fn, res)
+        #cv2.waitKey()
+        res = cv2.bitwise_not(res)
+        #cv2.imshow(fn, res)
+        #cv2.waitKey()
+        #res2 = cv2.bitwise_and(img,img, mask = res)
+        masks[fn] = res
+        #cv2.imshow(fn, res2)
+        #cv2.waitKey()
+    return masks
+
+def task5(images, descriptor): # tiene q devolver máscaras y descriptores
+    """
+    Extracts the background from the qsd2 images and computes the descriptors of the foreground
+
+    :param images: images we want to extact the background to
+    :param descriptor: kind of descriptor we want to extract from the images
+    :returns masks of segmented images and their descriptors
+    """
+
+    """ Code to plot 3D HSV graph and 3D RGB graph
+    for fn in images: 
+        plt.close('All')
+        img = images[fn]
+        r,g,b = cv2.split (img)
+        fig = plt.figure()
+        axis = fig.add_subplot(1, 1, 1, projection="3d")
+        pixel_colors = img.reshape((np.shape(img)[0]*np.shape(img)[1], 3))
+        norm = colors.Normalize(vmin=-1, vmax=1.)
+        norm.autoscale(pixel_colors)
+        pixel_colors = norm(pixel_colors).tolist()
+        axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, marker=".")
+        axis.set_xlabel("Red")
+        axis.set_ylabel("Green")
+        axis.set_zlabel("Blue")
+        plt.savefig('../results/'+fn+'_RGB.png')
+        plt.close(fig)
+
+        #HSV
+        hsv_img = transform_color(img, "HSV")
+        h, s, v = cv2.split(hsv_img)
+        fig = plt.figure()
+        axis = fig.add_subplot(1, 1, 1, projection="3d")
+
+        axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
+        axis.set_xlabel("Hue")
+        axis.set_ylabel("Saturation")
+        axis.set_zlabel("Value")
+        plt.savefig('../results/'+fn+'_HSV.png')
+        plt.close(fig)
+    """
+    src_images = {k:v for k,v in images.items() if 'jpg' in k}
+    masks = segment_paintings(src_images)
+    histograms = task1(src_images, descriptor, False, False, masks)
+    return masks, histograms
+
+""" ----- T6 - QS2 Evaluation  ----- """ # tiene q devolver precision, recall, f1 y map
+def task6(images, descriptor):
+    src_masks = {k:v for k,v in images.items() if 'png' in k}
+    src_images = {k:v for k,v in images.items() if 'jpg' in k}
+
+    new_masks = segment_paintings(src_images)
+    
+    precision = dict()
+    recall = dict()
+    f1 = dict()
+    tp = fp = falsen = 0
+    for fn in new_masks:
+        filename = os.path.splitext(fn)
+        new_mask = new_masks[fn]
+        src_mask = src_masks[filename[0]+'.png']
+        src_mask = transform_color(src_mask, 'Gray')
+
+        rows, cols = new_mask.shape
+
+        for i in range(rows):
+            for j in range(cols):
+                new_px = new_mask[i,j]
+                src_px = src_mask[i,j]
+                if new_px == (src_px == 255):
+                    tp += 1
+                elif (new_px != src_px) and (new_px == 255):
+                    fp +=1
+                elif (new_px != src_px) and (new_px == 0):
+                    falsen += 1
+
+        precision[fn] = tp / (tp + fp)
+        recall[fn] = tp / (tp + falsen)
+        f1[fn] = 2*precision[fn]*recall[fn] / (precision[fn]+recall[fn])
+    
+    return precision, recall, f1
--- W1/src/m1_w1.py
+++ W1/src/m1_w1.py
@@ -1,34 +1,34 @@
-
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """ MCV - M1:  Introduction to human and computer vision
     Week 1 - Content Based Image Retrieval
     Team 2 - Lali Bibilashvili
-             Víctor Casales
+             Victor Casales
              Jaume Pregonas
              Lena Tolstoy
 
     m1_w1.py: main program
 """
-import pickle
 
 """ Imports """
 import argparse
 import os
 import numpy as np
 import sys
+import cv2
 sys.path.append(os.getcwd()[:os.getcwd().index('src')])
 import src.functions as functions
+from pandas import Series
+
 
 """ Constants """
-DESCRIPTORS = ("some_descriptor", "another_descriptor") #TODO: add descriptors names
-MEASURES = ("some_measure", "another_measure") #TODO: add measures names
+DESCRIPTORS = ("1D_hist", "2D_hist", "3D_hist")
+#COLOR_SPACE = ("CieLAB", "YCbCr", "RGB")
+MEASURES = ("euclidean", "l1", "x2", "hist_intersection", "hellinger", "kl_divergence")
 
 """ Global variables """
-# here you can definde global variables -> IF NEEDED, try not to use them at all costs
 
 """ Classes """
-# if needed
 
 """ Functions """
 def build_arg_parser(ap):                       # here you can add all the flags you want our script to execute
@@ -37,13 +37,19 @@ def build_arg_parser(ap):                       # here you can add all the flags
     ap.add_argument("-t", "--task", required=True, dest="task", \
         help="number of the task to execute: 1-6")          
     ap.add_argument("-src", "--source", required=True, dest="src", \
-        help="path to the files to analyse: single image or folder with multiple images")
+        help="path to the folder with the images to analyse")
     ap.add_argument("-d", "--descriptor", required=False, dest="descriptor", \
         help="descriptor name, possible descriptors: " + str(DESCRIPTORS))
+    #ap.add_argument("-c", "--color", required=False, dest="color", \
+    #    help="color space, possible color spaces: " + str(COLOR_SPACE))
     ap.add_argument("-m", "--measure", required=False, dest="measure", \
         help="measure name, possible measures: " + str(MEASURES))
     ap.add_argument("-src2", "--source2", required=False, dest="src2", \
-        help="path to the museum images for task 3")
+        help="path to the  bbdd for task 3")
+    ap.add_argument("-plot", "--plot", required=False, dest="plot",\
+        help="allows plotting the results from the tasks")
+    ap.add_argument("-store", "--store", required=False, dest="store",\
+        help="stores the results from the tasks in the results folder (see documentation)")
 
 def load_images_from_folder(folder):
     images = dict()
@@ -70,90 +76,52 @@ def main():
     build_arg_parser(ap)
     args = ap.parse_args()
 
-    images = load_images_from_folder(args.src)        
-    histograms_bbdd = dict()
-    histograms_qsw1 = dict()
-    if args.task == "1": #convert color image to gray level
+    images = load_images_from_folder(args.src) 
+
+    if args.task == "1": #generates image descriptors (histograms)
+        if args.descriptor is None or args.descriptor not in DESCRIPTORS:
+            ap.error('A correct descriptor must be provided for task 1, possible descriptors: ' + str(DESCRIPTORS))
+        #elif args.descriptor == "3D_hist" and (args.color is None or  args.color not in COLOR_SPACE):
+        #    ap.error('A correct color space must be provided for 3D histograms, possible color spaces: ' + str(COLOR_SPACE))
+        else:
+            functions.task1(images, args.descriptor, False, True)
+
+    elif args.task == "2":
+        print("Nothing to show here, execute task 3")
+
+    elif args.task == "3":
+        if args.src2 is None:
+            ap.error('A source path with the museum images must be provided in order to execute task 3')
+        #elif args.descriptor is None or args.descriptor not in DESCRIPTORS:
+        #    ap.error('A correct descriptor must be provided for task 3, possible descriptors: ' + str(DESCRIPTORS))
+        elif args.measure is None or args.measure not in MEASURES:
+            ap.error('A correct measure must be provided for task 3, possible measures: ' + str(MEASURES))
+        else:
+            images_bbdd = load_images_from_folder(args.src2)
+            functions.task3(images_bbdd, images, args.measure)
+
+    elif args.task == "4":
+        if args.src2 is None:  
+            ap.error('A source path with the museum images must be provided in order to execute task 3')
+        elif args.measure is None or args.measure not in MEASURES:
+            ap.error('A correct measure must be provided for task 3, possible measures: ' + str(MEASURES))
+        else:
+            images_bbdd = load_images_from_folder(args.src2)
+            
+            functions.task4(images_bbdd, images, args.measure)
+            
+    elif args.task == "5":
         if args.descriptor is None or args.descriptor not in DESCRIPTORS:
             ap.error('A correct descriptor must be provided for task 1, possible descriptors: ' + str(DESCRIPTORS))
         else:
-            for img in images.keys():
-                gray = functions.transform_color(images[img],"Gray")
-
-    elif args.task == "2":
-        if args.measure is None or args.measure not in MEASURES:
-            ap.error('A correct measure must be provided for task 2, possible measures: ' + str(MEASURES))
-        else:
-            for img in images:
-                #cv2.imshow('image', img)
-                cv2.waitKey(0)
-                # TODO: call the function on functions.py and show the output to the user
-    elif args.task == "3":
-        if args.src2 is None: # TODO: also check if the path exists or if other conditions must be met
-            ap.error('A source path with the museum images must be provided in order to execute task 3')
-        else:
-            images2 = load_images_from_folder(args.src2)
-                
-            for img in images.keys(): #compute histogram of database
-                histograms_bbdd[img] = functions.compute_gray_histogram(img,images)
-
-            for img in images2.keys(): #compute the gray histograms of query set
-                histograms_qsw1[img] = functions.compute_gray_histogram(img,images2)
-
-    elif args.task == "4":
-        if args.src2 is None:  # TODO: also check if the path exists or if other conditions must be met
-            ap.error('A source path with the museum images must be provided in order to execute task 3')
-        else:
-            images2 = load_images_from_folder(args.src2)
-
-            for img in images.keys(): #compute the gray_histograms of database
-                histograms_bbdd[img] = functions.compute_gray_histogram(img,images)
-
-            for img in images2.keys(): #compute histograms of query set
-                histograms_qsw1[img] = functions.compute_gray_histogram(img,images2)
-            """
-                euclidean distance method
-            """
-            distances = functions.calculate_diferences(histograms_bbdd,histograms_qsw1,"euclidean") #compute the euclidean distance between each image of queryset and database
-            query,top_results = functions.get_top_k(distances,10) #get a top 10 results
-            with open('result.pkl', 'wb') as output:  # write the results in a file
-                pickle.dump([query,top_results], output)
-            with open("result.pkl", "rb") as fp:  # load the results in a file
-                [loaded_query,loaded_results] = pickle.load(fp)
-            #print(loaded_query,loaded_results)
-            """
-                l1 distance method
-            """
-
-            distances = functions.calculate_diferences(histograms_bbdd, histograms_qsw1,"l1_distance")  # compute the L1 distance between each image of queryset and database
-            query, top_results = functions.get_top_k(distances, 10)  # get a top 10 results
-            with open('result.pkl', 'wb') as output:  # write the results in a file
-                pickle.dump([query, top_results], output)
-            with open("result.pkl", "rb") as fp:  # load the results in a file
-                [loaded_query, loaded_results] = pickle.load(fp)
-
-            """
-                hellinger method
-            """
-            distances = functions.calculate_diferences(histograms_bbdd, histograms_qsw1,"hellinger")  # compute the L1 distance between each image of queryset and database
-            query, top_results = functions.get_top_k(distances, 10,True)  # get a top 10 results
-            with open('result.pkl', 'wb') as output:  # write the results in a file
-                pickle.dump([query, top_results], output)
-            with open("result.pkl", "rb") as fp:  # load the results in a file
-                [loaded_query, loaded_results] = pickle.load(fp)
-            print(loaded_query,loaded_results)
-    elif args.task == "5":
-        # TODO: check conditions
-        for img in images:
-            cv2.imshow('image', img)
-            cv2.waitKey(0)
-            # TODO: call the function on functions.py and show the output to the user
+            functions.task5(images, args.descriptor)
     elif args.task == "6":
-        # TODO: check conditions
-        for img in images:
-            cv2.imshow('image', img)
-            cv2.waitKey(0)
-            # TODO: call the function on functions.py and show the output to the user
+        precision, recall, f1 = functions.task6(images, args.descriptor)
+        avg_p = Series([precision.values()]).mean()
+        avg_r = Series([recall.values()]).mean()
+        avg_f1 = Series([f1.values()]).mean()
+        print("precision -> "+avg_p+", recall -> "+avg_r+", f1 -> "+avg_f1)
+
     else:
         ap.error("Task must be a number between 1 and 6")
 
